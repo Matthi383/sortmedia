ddc4cb1f47a34e1cc47e4f72efdc53fb8574a9cb
Sebastian Cruz <default50@gmail.com>
2021-04-20 09:32:02 +0100

    Take ownership of fork

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 from setuptools import setup, find_packages
 
 setup(
-    name='sortphotos',
-    version='1.0',
-    description='Organizes photos and videos into folders using date/time information ',
-    author='Andrew Ning',
+    name="sortphotos",
+    version="1.0",
+    description="Organizes photos and videos into folders using date/time information",
+    author="Sebastian Cruz",
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',
+    license="MIT License",
     entry_points={
-        'console_scripts': [
-          'sortphotos = src.sortphotos:main',
+        "console_scripts": [
+            "sortphotos = src.sortphotos:main",
         ]
-      }
+    },
 )
-
